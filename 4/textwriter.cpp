3e41aa2c18e0284540307cf112ae177330c901cb
MikeShah <mshah.475@gmail.com>
2021-05-13 13:30:44 -0400

    Live example

---
+++ 4/textwriter.cpp
@@ -0,0 +1,35 @@
+
+
+// g++ -std=c++17 -g textwriter.cpp -o prog
+// @file textwriter.cpp
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+int main(){
+
+    std::string line = "";
+
+    std::vector<std::string> lines;
+
+    std::cout << "Type: 'quit' to exit " << std::endl;
+    // Main loop, that reads in lines
+    while(line!="quit"){
+        std::cout << ": ";
+        //std::cin >> line;
+        std::getline(std::cin,line);
+        lines.push_back(line+'\n');
+    }
+
+    std::ofstream myFile("textWriterOutput.txt");
+    if(myFile.is_open()){
+        for(int i=0; i < lines.size(); i++){
+            myFile << lines[i];
+        }
+    }else{
+        std::cout << "Could not save file...sorry\n";
+    }
+
+    return 0;
+}
