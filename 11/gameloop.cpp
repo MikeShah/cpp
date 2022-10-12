444e956227b4991afef25c07b95a0b1facc155c1
MikeShah <mshah.475@gmail.com>
2022-10-11 22:46:19 -0400

    Game loop example

---
+++ 11/gameloop.cpp
@@ -0,0 +1,42 @@
+// g++ -g gameloop.cpp -o prog -lsfml-system -lsfml-graphics -lsfml-window  && ./prog
+// @file gameloop.cpp
+#include <SFML/Graphics.hpp>
+
+int main(){
+
+    // Main render window
+    sf::RenderWindow renderWindow(sf::VideoMode(400,400),"Game Loop Structure");
+    // Setup Objects
+    sf::CircleShape shape(5.0f);
+
+    // Main Application Loop
+    while(renderWindow.isOpen()){
+
+        // Handle Events
+        sf::Event event;
+        while(renderWindow.pollEvent(event)){
+            if(event.type == sf::Event::Closed){
+                renderWindow.close();
+            }
+        }
+ 
+        // Handle Inputs
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            sf::Vector2i mouse = sf::Mouse::getPosition(renderWindow);
+            shape.setPosition(mouse.x,mouse.y);
+        }
+        
+        // ======== Draw our scene =============
+        // Clear the window
+        renderWindow.clear();
+        // Draw the sprite
+        renderWindow.draw(shape);
+        // Display the contents drawn to the renderWindow
+        renderWindow.display();
+    }
+
+    return 0;
+}
+
+
+
