d5103634ff7f5dc9b32bee858a50d18f4540caeb
Mike <mike@Michaels-MacBook-Air.local>
2022-10-17 10:24:33 -0400

    added comments

--- 12/student.cpp
+++ 12/student.cpp
@@ -17,15 +17,16 @@ struct Student{
     }
 };
 
-
 // Entry point to program 'main' student.cpp
 int main(int argc, char* argv[]){
   
     Student s1; // Create a student (default constructor called)
+            s1.age = 21; // Set 'age' on this specific instance 's1'
+                         // of an object with type 'Student'
+
                 // Note: The default constructor is called behind
                 //       the scenes, we did not create it.
     s1.print(); // Print whatever default constructor assigns age to
-
 
     return 0;
 }
