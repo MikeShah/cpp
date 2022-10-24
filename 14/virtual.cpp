c1c573e13045579a3268168fa7d1d2443329042c
mike <mike@system76-pc.localdomain>
2022-10-24 17:04:11 -0400

    virtual comments

--- 14/virtual.cpp
+++ 14/virtual.cpp
@@ -28,11 +28,21 @@ class Rectangle: public Shape{
 	// 'override' is not required, but will
 	// signal to the compiler that you are indeed looking
 	// for a function in a parent class to override.
-    int area() override{
+    int area() override {
         std::cout << "Rectangle::area" << std::endl;
         return width*height;
     }
 };
+
+// The point of being able to use the 'base class'
+// Shape* is that we can then write functions which
+// take in the Base type (or return the base type)
+// and do the correct behavior for all things in the 
+// hierarchy for 'shape'
+void PerformOperation(Shape* shape){
+	shape->area();	
+}
+
 
 // Entry point to program 'main' virtual.cpp
 int main(){
@@ -43,6 +53,9 @@ int main(){
     // How about now?
     Shape* s2 = new Shape;
     s2->area();
+
+	PerformOperation(s1);
+	PerformOperation(s2);
 
     return 0;
 }
